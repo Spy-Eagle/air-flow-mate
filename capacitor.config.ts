1d0e9f95846751feaa3df27c7e82ed840b65c483
David Spychalski <dspy@spycor.com>
2025-08-20 20:32:50 -0400

    Update capacitor.config.ts
    Update: . Remove Development Server Config: Before building for production, remove the server config from capacitor.config.ts (lines 7-10)

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,10 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.spycor.airflowcalculator',
   appName: 'Spycor Air Flow Calculator',
   webDir: 'dist',
-  server: {
-    url: 'https://e13bc01c-5698-431f-bda3-398307c4ae65.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
   plugins: {
     SplashScreen: {
       launchShowDuration: 2000,
