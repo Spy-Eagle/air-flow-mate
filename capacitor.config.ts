f2ed7591686ccbb5fde4b9bc47080e9b85a3d0c4
David Spychalski <dspy@spycor.com>
2025-08-29 15:39:45 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,17 +4,6 @@ const config: CapacitorConfig = {
   appId: 'com.spycor.airflowcalculator',
   appName: 'Spycor Air Flow Calculator',
   webDir: 'dist',
-  server: {
-    url: 'https://e13bc01c-5698-431f-bda3-398307c4ae65.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 2000,
-      backgroundColor: '#1e40af',
-      showSpinner: false
-    }
-  }
-};
+  };
 
 export default config;
